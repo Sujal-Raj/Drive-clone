393654878730b871f40a38bc3724c8115de3326b
user.name like : "Sujal Raj <user.email like : "sujalraj2064@gmail.com>
2024-11-02 14:53:20 +0530

    Express validater added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userRouter = require("./routes/user.routes");
 
 app.set("view engine","ejs")
 app.use("/user",userRouter);
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index")
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));
 
 router.get("/",(req,res)=>{
     res.send("user routes page")
@@ -7,6 +11,15 @@ router.get("/",(req,res)=>{
 
 router.get("/register",(req,res)=>{
     res.render("register")
+})
+
+router.post("/register",
+    body("email").trim().isEmail().isLength({min:7}),
+    body("password").trim().isLength({min:5}),
+    body("username").trim().isLength({min:3}),
+    (req,res)=>{
+    console.log(req.body);
+    res.send("submitted");
 })
 
 module.exports = router
