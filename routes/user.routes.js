289078e47271b4e78ef90b225bd59c5e9e2f3762
user.name like : "Sujal Raj <user.email like : "sujalraj2064@gmail.com>
2024-12-01 13:26:20 +0530

    register and login completed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 router.use(express.json());
 router.use(express.urlencoded({extended:true}));
@@ -18,7 +19,7 @@ router.post("/register",
     body("email").trim().isEmail().isLength({min:7}),
     body("password").trim().isLength({min:5}),
     body("username").trim().isLength({min:3}),
-    (req,res)=>{
+    async (req,res)=>{
 
     const errors = validationResult(req);
 
@@ -29,7 +30,76 @@ router.post("/register",
         })
     }
 
+    const {username,password,email} = req.body;
+
+    const hashPassword = await bcrypt.hash(password,10);
+
+    const newUser = await userModel.create({
+        email:email,
+        username:username,
+        password:hashPassword,
+    })
+
+    res.json(newUser);
     
 })
 
-module.exports = router
+
+router.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+router.post("/login",
+    body("username").trim().isLength({min:3}),
+    body("password").trim().isLength({min:5}),
+
+    async(req,res)=>{
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:"Invalid data"
+            })
+        }
+        
+
+        const {username,password} = req.body;
+    
+        const user = await userModel.findOne({
+            username:username
+        })
+        // console.log("user found",user); //Debugging line
+        
+        if(!user){
+            return res.status(400).json({
+                message:"Username or password is incorrect"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        console.log("Password Match",isMatch);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Username or password is incorrect"
+            })
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            email:user.email,
+            username:user.username,
+        },
+            process.env.JWT_SECRET,
+        )
+
+        res.json({
+            token
+        })
+
+    }
+
+)
+
+module.exports = router  
